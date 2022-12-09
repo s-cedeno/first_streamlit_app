8e73cdc8fcd645e0db66ebae7f520111c15cb874
S. Cedeno <120052142+s-cedeno@users.noreply.github.com>
2022-12-09 16:44:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 
 #create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       return fruityvice_normalized
 
@@ -39,8 +39,10 @@ try:
       streamlit.error("Please select a fruit to get information.")
   else: 
       back_from_function = get_fruityvice_data(fruit_choice)
-      streamlit.dataframe (back_from_function)
-            
+      streamlit.dataframe(back_from_function)
+  
+      
+streamlit.stop()
 except URLError as e: 
     streamlit.error()
 
@@ -58,7 +60,6 @@ def insert_row_snowflake(new_fruit):
            my_cur.execute("insert into fruit_load_list values ('" +new_fruit+"')")
            return "Thanks for adding" + new_fruit
       
-streamlit.stop()      
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 if streamlit.button('Get Fruit Load List'):
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
